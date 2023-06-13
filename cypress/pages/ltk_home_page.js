872d5457ab96c73736c49a5752ec1c131adb24f4
Salim <salim@Salims-MacBook-Pro.local>
2023-06-12 21:18:29 -0700

    Cleaned some comments

--- cypress/pages/ltk_home_page.js
+++ cypress/pages/ltk_home_page.js
@@ -11,19 +11,6 @@ class HomePage {
         // Click on the Sign Up button
         cy.get('a[href="/signup"]').click();
     }
-
-
-  
-    // fillSignUpForm(email, password, firstName, lastName) {
-    //     cy.get('input[name="email"]').type(email);
-    //     cy.get('input[name="password"]').type(password);
-    //     cy.get('input[name="firstName"]').type(firstName);
-    //     cy.get('input[name="lastName"]').type(lastName);
-    // }
-  
-    // submitSignUpForm() {
-    //     cy.get('.signup-form__button').click();
-    // }
 }
 
 export default HomePage;
--- cypress/pages/ltk_sign_up_page.js
+++ cypress/pages/ltk_sign_up_page.js
@@ -4,13 +4,6 @@ class SignUpPage {
         cy.title().should('eq', 'Sign Up for an LTK Account | LTK')
     }
 
-    // fillSignUpForm(email, password, firstName, lastName) {
-    //     cy.get('input[name="email"]').type(email);
-    //     cy.get('input[name="password"]').type(password);
-    //     cy.get('input[name="firstName"]').type(firstName);
-    //     cy.get('input[name="lastName"]').type(lastName);
-    // }
-
     typeEmail() {
         let random = Math.ceil(Math.random()) + Math.random()
         cy.get('#input-15').type('ltk' + random + '@yopmail.com');
@@ -19,10 +12,6 @@ class SignUpPage {
     continueButton() {
         cy.get('span[class="v-btn__content"]').find('span').contains('continue').click();
     }
-  
-    // submitSignUpForm() {
-    //     cy.get('.signup-form__button').click();
-    // }
 
     typePassword(){
         cy.get('#input-34').type('password123');
